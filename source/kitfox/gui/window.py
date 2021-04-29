c1cfc4e63f1e0ca2546dd0f3685c2b40e09cc6d1
Mark McKay <mark@kitfox.com>
2021-04-29 06:07:52 -0400

    Now drawing title for window.

--- source/kitfox/gui/window.py
+++ source/kitfox/gui/window.py
@@ -27,16 +27,21 @@ from ..math.vecmath import *
 
 
 vertices = (
-    (100, 100), (300, 100),
-    (100, 200), (300, 200))
+    (0, 0), (1, 0),
+    (1, 1), (0, 1))
 
-indices = (
-    (0, 1, 2), (2, 1, 3))
+# vertices = (
+    # (100, 100), (300, 100),
+    # (100, 200), (300, 200))
+
+# indices = (
+    # (0, 1, 2), (2, 1, 3))
 
 shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
 #batch = batch_for_shader(shader, 'TRIS', {"pos": vertices}, indices=indices)
 
 batch = batch_for_shader(shader, 'TRI_FAN', {"pos": vertices})
+#batch = batch_for_shader(shader, 'TRI_FAN', {"pos": coordsSquare})
 
 #----------------------------------
 
@@ -70,11 +75,14 @@ class FoldoutPanel(Panel):
 
 class Window:
     def __init__(self):
-        self.position = Vector((0, 0))
+        self.position = Vector((100, 40))
         self.size = Vector((100, 100))
-        self.title = ""
+        self.title = "Untitled"
         self.titleHeight = 20
         self.background_color = Vector((.5, .5, .5, 1))
+        self.font_color = Vector((1, 1, 1, 1))
+        self.font_dpi = 20
+        self.font_size = 60
         
         self.dragging = False
         
@@ -85,19 +93,71 @@ class Window:
     def mouse_move(self, context, event):
         return {'consumed': False}
         pass
-        
-    def draw(self, context):
+      
+    def coords_to_screen_matrix(self, context):
         region = context.region
         #rv3d = context.region_data
-        region.width
-        region.x
+
+        mT = Matrix.Translation((0, region.height, 0))
+        mS = Matrix.Diagonal((1, -1, 1, 1))
+        return mT @ mS
+        
+    # def draw_string(self, context, string, pos):
+        # c2s = self.coords_to_screen_matrix(context)
+        
+        
+        
+    def draw(self, context):
+        bgl.glDisable(bgl.GL_DEPTH_TEST)
+    
+        mToScreen = self.coords_to_screen_matrix(context)
+        
+        
+        #self.draw_string(self.title, 
+        # region = context.region
+        # #rv3d = context.region_data
+
+        # mT = Matrix.Translation((0, region.height, 0))
+        # mS = Matrix.Diagonal((1, -1, 1, 1))
+        # mToScreen = mT @ mS
+
+
+        #Background
+        mT = Matrix.Translation(self.position.to_3d())
+        mS = Matrix.Diagonal(self.size.to_4d())
+        
+        m = mToScreen @ mT @ mS
+        
+#        print("window mtx " + str(m))
+        
+        gpu.matrix.push()
+        
+        gpu.matrix.multiply_matrix(m)
+
         
         shader.bind()
 #        shader.uniform_float("color", (0, 0.5, 0.5, 1.0))
         shader.uniform_float("color", self.background_color)
         batch.draw(shader)
         
-        #context.
+        gpu.matrix.pop()
+
+        #Text
+        font_id = 0  # default font
+        blf.color(font_id, self.font_color.x, self.font_color.y, self.font_color.z, self.font_color.w)
+        blf.size(font_id, self.font_size, self.font_dpi)
+        text_w, text_h = blf.dimensions(font_id, self.title)
+        
+        
+        width = self.size.x
+        text_x = self.position.x + (width - text_w) / 2
+        text_y = self.position.y + text_h
+        
+        screenPos = mToScreen @ Vector((text_x, text_y, 0, 1))
+        
+        blf.position(font_id, screenPos.x, screenPos.y, 2)
+        blf.draw(font_id, self.title)
+
         
     def handle_event(self, context, event):
         
