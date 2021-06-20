ffabb4006514c1c09bcc8a074c980c50bcffe34c
Mark McKay <mark@kitfox.com>
2021-06-20 14:01:35 -0400

    least_squares_fit now handles input of zero length array.

--- source/kitfox/gui/textInput.py
+++ source/kitfox/gui/textInput.py
@@ -35,6 +35,12 @@ from .label import *
 
 #----------------------------------
 
+class InputType(Enum):
+    DISPLAY = 0
+    TEXT_INPUT = 1
+
+#----------------------------------
+
 class TextInput(Label):
     def __init__(self, text = "label"):
         super().__init__(text)
@@ -42,6 +48,10 @@ class TextInput(Label):
         self.set_background_color(Vector((.7, .7, .7, 1)))
         self.set_border_radius(4)
         self.set_align_x(AlignX.RIGHT)
+        
+        #index of character cursor is just before
+        self.cursor_pos = 0
+        self.input_mode = InputType.DISPLAY
 
     # def handle_event(self, context, event):
         # panel_pos = self.get_screen_position()
@@ -51,6 +61,25 @@ class TextInput(Label):
             # print ("TextInput press")
             
         # return False
+
+    def draw_component(self, ctx):
+        super().draw_component(ctx)
+        
+        if self.input_mode == InputType.TEXT_INPUT:
+        
+            pass
+
+    def draw_cursor(self, ctx):
+        first_part = self.text[self.cursor_pos]
+        last_part = self.text[self.cursor_pos:]
+    
+        blf.size(self.font_id, self.font_size, self.font_dpi)
+        text_first_w, text_first_h = blf.dimensions(self.font_id, first_part)
+        text_last_w, text_last_h = blf.dimensions(self.font_id, last_part)
+        
+        
+        
+        bounds = self.bounds()
 
     def mouse_pressed(self, event):
         print ("TextINput pressed")
